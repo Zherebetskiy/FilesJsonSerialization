d0c97f06b4732a9c6e242566c39342a0acb16726
Yaroslav <zherebetskiy00@gmail.com>
2018-08-21 20:54:44 +0300

    added object creator for folders tree

---
+++ FilesJsonSerializationLibrary/FilesJsonSerializationLibrary/Services/FileSystemCreator.cs
@@ -0,0 +1,82 @@
+﻿using FilesJsonSerializationLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesJsonSerializationLibrary.Services
+{
+    public class FileSystemCreator
+    {
+        //public string NeedlessDirectory { get; set; }
+        public string RootFolder { get; set; }
+
+        public FileSystemCreator(string rootFolder)
+        {
+            //   NeedlessDirectory = targetDirectory.Substring(0, targetDirectory.LastIndexOf("/")).Replace("/", "\\");
+            RootFolder = rootFolder;
+        }
+
+        //
+        // return List of custom File instances in target directory 
+        //
+        List<File> GetFiles(string rootFolder)
+        {
+            List<File> currentFiles = new List<File>();
+            var files = System.IO.Directory.GetFiles(rootFolder);
+
+            foreach (var file in files)
+            {
+                var info = new System.IO.FileInfo(file);
+                currentFiles.Add(new File
+                {
+                    Name = info.Name,
+                    Size = info.Length,
+                    Path = info.FullName
+                });
+            }
+
+            return currentFiles;
+        }
+
+        //
+        // recursively create folders' tree as custom Directory instances
+        //
+        List<Folder> GetFolders(string rootDirectory)
+        {
+            List<Folder> currentFolders = new List<Folder>();
+            var directories = System.IO.Directory.GetDirectories(rootDirectory);
+
+            foreach (var directory in directories)
+            {
+                var info = new System.IO.DirectoryInfo(directory);
+                currentFolders.Add(new Folder
+                {
+                    Name = info.Name,
+                    DateCreated = info.CreationTime,
+                    Files = GetFiles(directory),
+                    Children = GetFolders(directory)
+                });
+            }
+
+            return currentFolders;
+        }
+
+        //
+        // method for creating root directory and starting
+        // creation folders' tree
+        //
+        public Folder Create()
+        {
+            var info = new System.IO.DirectoryInfo(RootFolder);
+            return new Folder
+                {
+                    Name = info.Name,
+                    DateCreated = info.CreationTime,
+                    Files = GetFiles(RootFolder),
+                    Children = GetFolders(RootFolder)
+                };
+        }
+    }
+}
