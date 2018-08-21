b7bcf69a6f20ac9b411aaaf4b5cbf2e474e58a66
Yaroslav <zherebetskiy00@gmail.com>
2018-08-21 20:24:23 +0300

    added library and created models

---
+++ FilesJsonSerializationLibrary/FilesJsonSerializationLibrary/Models/File.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesJsonSerializationLibrary.Models
+{
+    public class File
+    {
+        public string Name { get; set; }
+        public double Size { get; set; }
+        public string Path { get; set; }
+    }
+}
---
+++ FilesJsonSerializationLibrary/FilesJsonSerializationLibrary/Models/Folder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesJsonSerializationLibrary.Models
+{
+    public class Folder
+    {
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+        public List<File> Files { get; set; }
+        public List<Folder> Children { get; set; }
+    }
+}
